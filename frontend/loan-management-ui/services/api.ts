0d06d33ab1958bdcb7afa0c21b4fec7d723be5ab
Pizzou <pmpumuropizzou@gmail.com>
2026-07-27 12:53:29 +0200

    doooo

--- frontend/loan-management-ui/services/api.ts
+++ frontend/loan-management-ui/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://fintech01.onrender.com/api',
   timeout: 20000,
   headers: { 'Content-Type': 'application/json' },
 });
